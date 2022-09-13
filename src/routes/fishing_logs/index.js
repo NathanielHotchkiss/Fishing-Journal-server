d0ba97b99249f2789bd4752a6f72bee94b4bc40d
NathanielHotchkiss <nathanielhotchkiss@gmail.com>
2022-09-12 23:16:30 -0400

    organized routes

--- src/routes/fishing_logs/index.js
+++ src/routes/fishing_logs/index.js
@@ -7,32 +7,69 @@ const jsonBodyParser = express.json();
 const router = express.Router();
 
 router
+  .route("/")
 
-  .get("/", async (req, res) => {
-    const { rows: fishing_logs } = await db.file("db/fishing_logs/get_all.sql");
-    res.json(fishing_logs);
-  })
-
-  .get("/:user_id", async (req, res) => {
-    const { user_id } = req.params;
+  .get(async (req, res) => {
+    const { user_id } = req.body;
+    console.log(user_id);
     const { rows: fishing_logs } = await db.file(
-      "db/fishing_logs/get_by_id.sql",
+      "db/fishing_logs/get_all_by_user.sql",
       { user_id }
     );
     res.json(fishing_logs);
   })
 
-  .delete("/:fish_id", async (req, res, next) => {
+  .post(jsonBodyParser, async (req, res, next) => {
+    const {
+      user_id,
+      species,
+      fish_length,
+      pounds,
+      ounces,
+      bait,
+      fishing_method,
+    } = req.body;
+
+    for (const field of ["species", "fish_length", "pounds", "ounces"])
+      if (!req.body[field])
+        return res.status(400).json({
+          error: `Missing '${field}' in request body`,
+        });
+    try {
+      const newLog = {
+        user_id,
+        species,
+        fish_length,
+        pounds,
+        ounces,
+        bait,
+        fishing_method,
+      };
+
+      const {
+        rows: [fishing_logs],
+      } = await db.file("db/fishing_logs/post.sql", newLog);
+
+      res.status(201).json(fishing_logs);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+router
+  .route("/:fish_id")
+  .all(checkLogExists)
+
+  .get(async (req, res) => {
     const { fish_id } = req.params;
-    await db
-      .file("db/fishing_logs/delete.sql", { fish_id })
-      .then(() => {
-        res.status(204).end();
-      })
-      .catch(next);
+    const {
+      rows: [fishing_logs],
+    } = await db.file("db/fishing_logs/get_fish.sql", { fish_id });
+    console.log(fishing_logs);
+    res.json(fishing_logs);
   })
 
-  .put("/:fish_id", jsonBodyParser, async (req, res, next) => {
+  .put(jsonBodyParser, async (req, res, next) => {
     const { fish_id } = req.params;
 
     console.log("req.body: ", req.body);
@@ -75,50 +112,30 @@ router
     }
   })
 
-  .get("/get/:fish_id", async (req, res) => {
+  .delete(async (req, res, next) => {
     const { fish_id } = req.params;
-    const {
-      rows: [fishing_logs],
-    } = await db.file("db/fishing_logs/get_by_id_fish.sql", { fish_id });
-    console.log(fishing_logs);
-    res.json(fishing_logs);
-  })
-
-  .post("/new", jsonBodyParser, async (req, res, next) => {
-    const {
-      user_id,
-      species,
-      fish_length,
-      pounds,
-      ounces,
-      bait,
-      fishing_method,
-    } = req.body;
-
-    for (const field of ["species", "fish_length", "pounds", "ounces"])
-      if (!req.body[field])
-        return res.status(400).json({
-          error: `Missing '${field}' in request body`,
-        });
-    try {
-      const newLog = {
-        user_id,
-        species,
-        fish_length,
-        pounds,
-        ounces,
-        bait,
-        fishing_method,
-      };
-
-      const {
-        rows: [fishing_logs],
-      } = await db.file("db/fishing_logs/post.sql", newLog);
-
-      res.status(201).json(fishing_logs);
-    } catch (error) {
-      next(error);
-    }
+    await db
+      .file("db/fishing_logs/delete.sql", { fish_id })
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch(next);
   });
+
+async function checkLogExists(req, res, next) {
+  const { fish_id } = req.params;
+  try {
+    const fishingLog = await db.file("db/fishing_logs/put.sql", fish_id);
+    if (!fishingLog) {
+      return res.status(404).json({
+        error: "Log does not exist",
+      });
+    }
+    res.fishingLog = fishingLog;
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
 
 module.exports = router;
